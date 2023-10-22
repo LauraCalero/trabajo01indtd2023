205518b33d323d62580c2d2fef40f2382d96e619
Laura <lauracaleropalomo@gmail.com>
2023-10-22 16:33:23 +0200

    Enunciado del primer problema donde con el comando matrix() reflejamos bien en dos tablas, los datos que nos proporcionan

--- Enunciados.R
+++ Enunciados.R
@@ -1,8 +1,19 @@
 
 
-# LAURA CALERO PALOMO 
+#                         LAURA CALERO PALOMO 
+
 
 # ENUNCIADOS DEL TRABAJO DE TEORÍA DE LA DECISIÓN 
 
-# PROBLEMA 1: 
+# PROBLEMA 1: Aplicar los criterios de decisión bajo incertidumbre a los 
+# problemas cuya matriz de valores numéricos vienen dadas en las siguientes 
+# tablas:
+
+# (a) Tabla de beneficios (favorable)
+
+matrix(c(4,3,6,7,5,5,3,-2,4,6,-9,9,2,5,9,0,1,2,5,3),5,4)
+
+# (b) Tabla de costos (desfavorable)
+
+matrix(c(3,2,6,7,9,0,0,-1,-3,5,-5,4,4,6,7,0,2,2,1,0,9,8,7,3,4,4,-2,-1,5,-8),6,5)
 
