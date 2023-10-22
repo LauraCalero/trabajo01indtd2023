835fc16a930d336a803f7853a23859186a0831a8
Laura <lauracaleropalomo@gmail.com>
2023-10-22 20:37:58 +0200

    Corrección de todo el problema 1, debido a que entendí que era una tabla para cada situación (favorable, no favorable) y es sólo una

--- Enunciados.R
+++ Enunciados.R
@@ -5,17 +5,13 @@
 
 # ENUNCIADOS DEL TRABAJO DE TEORÍA DE LA DECISIÓN 
 
-# PROBLEMA 1: Aplicar los criterios de decisión bajo incertidumbre a los 
-# problemas cuya matriz de valores numéricos vienen dadas en las siguientes 
-# tablas:
-
-# (a) Tabla de beneficios (favorable)
+# PROBLEMA 1: Aplicar los criterios de decisión bajo incertidumbre por separado
+# a los problemas cuya matriz de valores numéricos viene dada en la siguiente 
+# tabla:
 
 matrix(c(4,3,6,7,5,5,3,-2,4,6,-9,9,2,5,9,0,1,2,5,3),5,4)
 
-# (b) Tabla de costos (desfavorable)
-
-matrix(c(3,2,6,7,9,0,0,-1,-3,5,-5,4,4,6,7,0,2,2,1,0,9,8,7,3,4,4,-2,-1,5,-8),6,5)
+# Hay que diferenciar entre casos favorables y casos no favorables.
 
 
 # PROBLEMA 2: Estamos pensado en ir a celebrar mi cumpleaños, mi mejor amiga
